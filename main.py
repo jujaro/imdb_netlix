a9e30d6a7c9a1d7647d9a6de84f2e5710e1d3c83
Juan Romero <juan.romero@intelematics.com>
2020-08-13 07:14:00 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+imdb  = pd.read_html('https://www.imdb.com/chart/top/?ref_=nv_mv_250')[0]
+imdb = imdb['Rank & Title'].str.extract('[0-9]+(.*)')[0].str.rsplit(' ',1,expand=True)[0].str.split(' ',1,expand=True)[1].str.strip()
+
+nf = pd.read_html('https://www.finder.com.au/netflix-movies')[0]
+
+print(set(nf.Title).intersection(set(imdb)))
+
